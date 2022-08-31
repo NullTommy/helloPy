d57415497e7b61aad11314a7d2fccc80c430d41c
不无聊 <NullTommy@163.com>
2022-08-31 11:19:38 +0800

    【脚本优化】完善脚本-自动生成标签脚本

--- test/test2.py
+++ test/test2.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*
 import time
 
-print('数据画像新人打标自动生成-开始')
+print('new person tag param bulid-begin')
 baseDemo = '{"data":[{"key":"feepay","value":0}],"dataId":"86_572950","modelCode":"vender","time":1660896307228}'
 base1 = '{"data":[{"key":"o2o_global_sell_new_user_pay","value":0}],"dataId":"'
 base2 = '","modelCode":"user","time":'
 base3 = '}'
 timeStr = str(int(time.time()) * 1000)
-dataId = raw_input('input dataId:')
+dataId = raw_input('plz input dataId:')
 print(base1 + str(dataId) + base2 + timeStr + base3)
-print('数据画像新人打标自动生成-结束。任意点击推出界面~~')
+print('new person tag param bulid-end. click anywhere to exit~~')
 dataId = raw_input()
 # 1660902351605
 # 1660902389000
